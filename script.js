7461154417b8be6e75ea4d30754ee67c2a5db659
Jesus Nader <nadergomezj@gmail.com>
2024-02-08 11:23:22 -0500

    Implementacion funcional del boton menu en mobile

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuDesktop = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
 
 function toggleDesktopMenu() {
     menuDesktop.classList.toggle('inactive');
 }
 
+function toggleMobileMenu() {
+    menuMobile.classList.toggle('inactive');
+}
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
