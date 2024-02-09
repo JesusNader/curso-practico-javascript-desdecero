3264a9db3f7d3e7d5a04d1ec102b1bbe2619ba3a
Jesus Nader <nadergomezj@gmail.com>
2024-02-09 14:13:23 -0500

    Codigo implementado para crear cards de productos

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const menuEmail = document.querySelector('.navbar-email');
-const menuDesktop = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.menu');
-const menuCartIcon = document.querySelector('.navbar-shopping-cart');
-const menuMobile = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
-
 function toggleDesktopMenu() {
     const isCartMenuClosed = aside.classList.contains('inactive');
 
@@ -22,6 +15,7 @@ function toggleMobileMenu() {
     }
     menuMobile.classList.toggle('inactive');
 }
+
 function toggleCartMenu() {
     const isMobileMenuClosed = menuMobile.classList.contains('inactive');
     const isDeskstopMenuClosed = menuDesktop.classList.contains('inactive');
@@ -36,6 +30,70 @@ function toggleCartMenu() {
     aside.classList.toggle('inactive');
 }
 
+function renderProducts(arr){
+    for(product of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+    
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+    
+        const productInfoDiv = document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', '/assets/icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+const menuEmail = document.querySelector('.navbar-email');
+const menuDesktop = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuCartIcon = document.querySelector('.navbar-shopping-cart');
+const menuMobile = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+const productList = [
+    {
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+},
+{
+    name: 'Windows',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+},
+{
+    name: 'Computer',
+    price: 620,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+}
+];
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCartIcon.addEventListener('click', toggleCartMenu);
+renderProducts(productList)
