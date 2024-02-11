5d6de57bdd3feb3f2efb885c4c7f3fcaa62884cc
Jesus Nader <nadergomezj@gmail.com>
2024-02-11 00:04:06 -0500

    Mejoras visuales con animaciones a las cards

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ const productDetailContainer = document.querySelector('#product-detail');
 const cardsContainer = document.querySelector('.cards-container');
 
 const productList = [
-    {
+{
     name: 'Bike',
     price: 120,
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
